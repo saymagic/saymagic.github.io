f9665147f822e47a46ab6137567ece80e2ee89fb
saymagic <saymagic.dev@gmail.com>
2024-12-01 00:05:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """## [Gitblog](https://yihong0618.github.io/gitblog/)
-My personal blog([About Me](https://github.com/yihong0618/gitblog/issues/282)) using issues and GitHub Actions (随意转载，无需署名)
-![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
+MD_HEAD = """## [saymagic blog](https://blog.saymagic.cn/)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
